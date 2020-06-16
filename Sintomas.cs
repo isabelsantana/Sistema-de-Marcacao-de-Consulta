1d18d604a3facec2224d3e30fe3207f1b6d82a07
Isabel Santana <isabelsantana@ucl.br>
2020-06-16 23:28:35 +0000

    criação de dicionario

--- Sintomas.cs
+++ Sintomas.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 class Sintomas{
 
-  protected string descricao;
+  /*protected string descricao;
   protected string triagem;
 
   public string Descricao{
@@ -22,30 +23,39 @@ class Sintomas{
     set{
       triagem = value;
     } 
-  }   
+  }  */ 
 
   public void ColetarSintomas(){ // método para coletar os sintomas do usuário 
-
-    int[] sint = new int [5];
-    
 
     string[] lines = File.ReadAllLines("sintomas.txt");
       foreach(var line in lines) Console.WriteLine(line);
 
-    Console.WriteLine("Informe os sintomas: ");
+    Console.WriteLine("\nInforme o código dos sintomas: ");
 
-    for (int i = 0; i < 5 ; i++){
-      sint[i] = int.Parse(Console.ReadLine());
+    Dictionary <int, string> sintoma = new Dictionary <int,string>();  
+
+    sintoma.Add(1,"Dores locais nas articulações");
+    sintoma.Add(2,"Tosse com catarro, seca, forte ou leve");
+    sintoma.Add(3,"Calafrios");    
+    sintoma.Add(4,"Febre");
+    sintoma.Add(5,"Perda de Apetite");
+    sintoma.Add(6,"Dor de gargangta ou irritação");
+    sintoma.Add(7,"Dor de cabeça");
+    sintoma.Add(8,"Falta de ar");
+    sintoma.Add(9,"Dor ao movimentar os olhos");
+    sintoma.Add(10,"Manchas vermelhas no corpo");
+    sintoma.Add(11,"Dores musculares intensas");
+    sintoma.Add(12,"Mal estar");
+    sintoma.Add(13,"Chiado no peito");
+    sintoma.Add(14,"Desconforto torácico");
+    sintoma.Add(15,"Ansiedade");
+
+    foreach (KeyValuePair <int, string> sint in sintoma){
+      Console.WriteLine(sint.Key + " - " + sint.Value);
     }
-
-    Console.WriteLine("Os sintomas apresentados:{0} ",sint[5]);
-      
-    
-
- 
-  }  
+  }
+}  
 
 
 
-}
 
